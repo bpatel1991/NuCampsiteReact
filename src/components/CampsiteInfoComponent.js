632e3f714ad4d54c0163ce6d34ce88a47c0e2542
Bhavini Tina Patel <bhavini.patel91@gmail.com>
2021-03-23 21:13:21 -0700

    Redux Actions

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -21,7 +21,7 @@ function RenderCampsite({campsite}) {
     );
 }
 
-function RenderComments({comments}) {
+function RenderComments({comments, addComment, campsiteId}) {
     if (comments) {
         return (
             <div className="col-md-5 m-1">
@@ -38,7 +38,7 @@ function RenderComments({comments}) {
                         </div>
                     );
                 })}
-                <CommentForm/>            
+                <CommentForm campsiteId={campsiteId} addComment={addComment} />           
             </div>
         );
     }
@@ -61,7 +61,11 @@ function CampsiteInfo(props) {
                 </div>
                 <div className="row">
                     <RenderCampsite campsite={props.campsite} />
-                    <RenderComments comments={props.comments} />
+                    <RenderComments 
+                        comments={props.comments}
+                        addComment={props.addComment}
+                        campsiteId={props.campsite.id}
+                    />
                 </div>
             </div>
         );
@@ -97,8 +101,8 @@ class CommentForm extends Component {
     }
 
     handleSubmit(values) {
-        console.log('Current state is: ' + JSON.stringify(values));
-        alert(`Thank you for submitting! \n Rating: ${values.rating} \n Author: ${values.author} \n Comment: ${values.text}`);
+        this.toggleModal();
+        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
     }
 
     render() {
