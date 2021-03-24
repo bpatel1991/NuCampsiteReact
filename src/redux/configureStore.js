4ad0e132951ebc44931fac4a8731e926d9cc5459
Bhavini Tina Patel <bhavini.patel91@gmail.com>
2021-03-23 22:58:07 -0700

    React Redux Forms Revisited

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,12 @@
 import {createStore, combineReducers, applyMiddleware } from 'redux';
+import { createForms } from 'react-redux-form';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import { Campsites } from './campsites';
 import { Comments } from './comments';
 import { Partners } from './partners';
 import { Promotions } from './promotions';
+import { InitialFeedback } from './forms';
 
 
 export const ConfigureStore = () => {
@@ -13,8 +15,11 @@ export const ConfigureStore = () => {
             campsites: Campsites,
             comments: Comments,
             partners: Partners,
-            promotions: Promotions
-        }),
+            promotions: Promotions,
+            ...createForms({   //passed as an argument using spread syntax//
+                feedbackForm: InitialFeedback  //model name is feedbackForm, give it that initial feedback object for the initial form state//
+            })
+        }),  
         applyMiddleware(thunk, logger)
 
     );
---
+++ src/redux/forms.js
@@ -0,0 +1,13 @@
+export const InitialFeedback = {
+    firstName: '',
+    lastName: '',
+    phoneNum: '',
+    email: '',
+    agree: false,
+    contactType: 'Phone',
+    feedback: ''
+};
+
+//export object that holds initial state of contact form//
+//used to reset form later//
+//these correspond to .model in contactcomponent//
